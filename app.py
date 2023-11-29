3cec4f1b4372a38ab17caba851eebf674589a39d
Muhammad Rameez <mrameez820@gmail.com>
2023-11-29 12:52:36 +0500

    template added newupd

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, render_template,request
 import requests
 
 app = Flask(__name__)
