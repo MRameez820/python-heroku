1e1b514f234184c69eb0153b416a58b7e0a8b0b6
Muhammad Rameez <mrameez820@gmail.com>
2023-11-29 13:08:31 +0500

    template updated

--- app.py
+++ app.py
@@ -5,9 +5,65 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+
+def get_html(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # This will raise an exception for HTTP errors
+        return response.text
+    except requests.RequestException as e:
+        print("Error fetching the URL: {e}")
+        return None
+
+
+
+def findTitle(url):
+  html_content = get_html(url)
+  beginningTitleIndex = html_content.find("<title>")
+  endTitleIndex = html_content.find("</title")
+  title = html_content[beginningTitleIndex + 7 : endTitleIndex]
+  return title
+
+
+
+def analyzePageTitle(title,url):
+  titleLength = len(title)
+  titleWords = title.split()
+  titleWordCount = len(titleWords)
+  domainName = url.split('//')[1].split('/')[0]
+  lowercaseDomainName = domainName.lower()
+  wordsInDomain = []
+  for word in titleWords:
+    if word.lower() in lowercaseDomainName:
+       wordsInDomain.append(word)
+  domainCount = len(wordsInDomain)
+  finret = "Domain : " + url +  " Title : " + title +  " Title Length : " + str(titleLength) + " Word Count : " + str(titleWordCount) + " Words in domain : " + str(domainCount)
+  return finret
+
+
+def submitclick():
+    return 'clicked'
+
+@app.route("/")
+def hello():
+    url = "https://polygonsmedia.com/"  
+    version = findTitle(url)
+    message = analyzePageTitle(version,url)
     return render_template('index.html')
+
+
+@app.route('/results')
+def showres():
+    domain = request.args.get('domain')
+    version = findTitle(domain)
+    message = analyzePageTitle(version,domain)
+    return render_template('results.html' , value=message)
+      
+    
+@app.route("/<string:name>/")
+def say_hello(name):
+    return f"Hello {name}!"
+    
 
 if __name__ == '__main__':
     app.run()
