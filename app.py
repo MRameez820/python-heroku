3dac48f66aaf0bdfeeeb62112d01ca3cd05daa6d
Muhammad Rameez <mrameez820@gmail.com>
2023-11-29 12:50:04 +0500

    template added

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! new'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
